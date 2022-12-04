39262a36cffcf2a6a3e31e2abba574868d2af253
Lucas <45669808+Lulutorus@users.noreply.github.com>
2022-12-04 16:59:01 -0500

    Revert "Merge branch 'imagecolors'"
    This reverts commit bd6812eed7d084ab722f61673b0caefbe7784bac, reversing
    changes made to beac47fe0524c46bda10a3d853c172a5093b63d7.

--- app/src/main/java/com/example/convertisseurdecouleur/MainActivity.java
+++ app/src/main/java/com/example/convertisseurdecouleur/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.convertisseurdecouleur;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,11 +12,6 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,26 +20,9 @@ public class MainActivity extends AppCompatActivity {
     //import des fragments
 
 
-    private static final int PERMISSIONS_REQUEST = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Verification qu'on a la permission d'ouvrir le stockage
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-                && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // Demande de permission sinon
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST);
-        }
-
-        // Dynamic fragment switch
-        //if (savedInstanceState == null) {
-        //    getSupportFragmentManager().beginTransaction()
-        //            .setReorderingAllowed(true)
-        //            .add(R.id.fragment_container_view, ImageColorsFragment.class, null)
-        //            .commit();
-        //}
         setContentView(R.layout.activity_main);
 
         Button changeImage = findViewById(R.id.buttonImage);
@@ -90,20 +67,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
     }
-
-    // Toaster for permissions
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST:
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-        }
-    }
-
 }
