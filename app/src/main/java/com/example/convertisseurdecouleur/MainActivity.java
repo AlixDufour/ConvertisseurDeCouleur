5b20a8185f283f1cc9bffb7e2e91210a81a47315
Lucas <45669808+Lulutorus@users.noreply.github.com>
2022-12-04 21:06:35 -0500

    Revert "Revert "Merge branch 'lucas'""
    This reverts commit 4dae43a1055f2502e3ac4e53a4c91a577ad29f47.

--- app/src/main/java/com/example/convertisseurdecouleur/MainActivity.java
+++ app/src/main/java/com/example/convertisseurdecouleur/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.convertisseurdecouleur;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,9 +8,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button changeActivity;
+    BottomNavigationView bottomNavigationView;
+
+    //import des fragments
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +43,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(change);
             }
         });
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //fragment de base (surement convertisseur)
+        //getSupportFragmentManager().beginTransaction().replace(R.id.container, 'fragment').commit();
+
+        /*
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case fragment convert :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, 'convert').commit();
+                        return true;
+                    case fragment picker :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, 'picker').commit();
+                        return true;
+                    case fragment image color :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, 'image color').commit();
+                        return true;
+                }
+                return false;
+            }
+        });*/
     }
 }
